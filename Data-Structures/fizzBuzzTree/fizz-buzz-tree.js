935064cb81126ed993610915b95e23ffedd780e6
osamamousa204 <osamamousa204@gmail.com>
2020-06-15 00:28:35 +0300

    added edge caces

--- Data-Structures/fizzBuzzTree/fizz-buzz-tree.js
+++ Data-Structures/fizzBuzzTree/fizz-buzz-tree.js
@@ -90,20 +90,28 @@ myTree.add(21);
   
 
 function FizzBuzzTree (tree) {
-  const result = tree;
-  const _traversal = (node)=>{
-    if(!(node.value % 5) && !(node.value % 3)){
-      node.value = 'FizzBuzz';
-    }else if (!(node.value % 5)){
-      node.value = 'Buzz';
-    }else if(!(node.value % 3)){
-      node.value = 'Fizz';
+  if(tree){
+    if(tree.root){
+      const result = tree;
+      const _traversal = (node)=>{
+        if(!(node.value % 5) && !(node.value % 3)){
+          node.value = 'FizzBuzz';
+        }else if (!(node.value % 5)){
+          node.value = 'Buzz';
+        }else if(!(node.value % 3)){
+          node.value = 'Fizz';
+        }else{
+          node.value = `${node.value}`;
+        }
+        if(node.left) _traversal (node.left);
+        if(node.right) _traversal (node.right);    
+      };
+      _traversal(result.root);
+      return result;
     }else{
-      node.value = `${node.value}`;
+      return 'the tree is empty';
     }
-    if(node.left) _traversal (node.left);
-    if(node.right) _traversal (node.right);    
-  };
-  _traversal(result.root);
-  return result;
+  }else{
+    return 'please pass a tree';
+  }
 }
